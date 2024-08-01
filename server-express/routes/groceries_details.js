e704369e02f1665ef89a0c77ba2bc50cac668a4f
Jean Waston Therane <89857594+jewathe@users.noreply.github.com>
2024-08-01 12:11:34 -0400

    update

--- server-express/routes/groceries_details.js
+++
@@ -1,8 +0,0 @@
-import { getGroceriesDetails } from '../controller/groceries_details.js'
-import express from 'express'
-const router = express.Router()
-
-/* READ */
-router.get('/', getGroceriesDetails)
-
-export default router
